6f3a05530fe2f43b223e5a39bc5108e1bb93556a
Anders Bech Mellson <mellson@icloud.com>
2022-03-23 14:52:18 +0100

    Settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { InferGetServerSidePropsType, NextPage } from "next";
+import type { InferGetServerSidePropsType } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
