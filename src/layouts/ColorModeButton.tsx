99ab8f29f73dc03d20fd24bcee2aec72fd8e49da
Anders Bech Mellson <mellson@icloud.com>
2022-03-25 09:00:38 +0100

    Dark / light switch

--- src/layouts/ColorModeButton.tsx
+++ src/layouts/ColorModeButton.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Icon, Text, useColorMode } from "@chakra-ui/react";
+import { Button, Icon, useColorMode } from "@chakra-ui/react";
 import * as React from "react";
 import { FiMoon, FiSun } from "react-icons/fi";
 
@@ -7,14 +7,11 @@ export function ColorModeButton() {
 
   return (
     <Button variant="ghost" justifyContent="start" onClick={toggleColorMode}>
-      <HStack spacing="3">
-        <Icon
-          as={colorMode === "light" ? FiMoon : FiSun}
-          boxSize="6"
-          color="subtle"
-        />
-        <Text>{colorMode === "light" ? "Sluk lyset" : "Tænd lyset"}</Text>
-      </HStack>
+      <Icon
+        as={colorMode === "light" ? FiMoon : FiSun}
+        boxSize="6"
+        color="subtle"
+      />
     </Button>
   );
 }
