e7e7ce961f31697ceeac8b88c6c2eae35f2c06e0
Anders Bech Mellson <mellson@icloud.com>
2022-03-27 14:13:05 +0200

    Profil og Logo link

---
+++ pages/profil.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+function Om() {
+  return (
+    <>
+      <Heading size="md">Profil</Heading>
+    </>
+  );
+}
+
+export default Om;
--- src/layouts/Logo.tsx
+++ src/layouts/Logo.tsx
@@ -1,11 +1,17 @@
+import { Link } from "@chakra-ui/layout";
 import { Heading, useBreakpointValue } from "@chakra-ui/react";
+import NextLink from "next/link";
 import * as React from "react";
 
 export const Logo = () => (
-  <Heading
-    size={useBreakpointValue({ base: "xs", lg: "sm" })}
-    fontWeight="bold"
-  >
-    Leanders 🥐
-  </Heading>
+  <NextLink href="/" passHref>
+    <Link>
+      <Heading
+        size={useBreakpointValue({ base: "xs", lg: "sm" })}
+        fontWeight="bold"
+      >
+        Leanders 🥐
+      </Heading>
+    </Link>
+  </NextLink>
 );
