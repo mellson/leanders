aa2e4ed6c5daabb0f6004637a19928ca3f4b63b4
Anders Bech Mellson <mellson@icloud.com>
2022-03-29 11:21:22 +0200

    Dark mode fix?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,15 @@
-import { theme } from "@chakra-ui/pro-theme";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "@fontsource/inter/variable.css";
 import type { AppProps } from "next/app";
 import { ChakraColorWrapper } from "../src/components/ChakraColorWrapper";
 import { AppLayout } from "../src/layouts/AppLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const myTheme = extendTheme(
-    {
-      colors: { ...theme.colors, brand: theme.colors.purple },
-    },
-    theme
-  );
   return (
-    <ChakraProvider theme={myTheme}>
-      <ChakraColorWrapper cookies={pageProps.cookies}>
-        <AppLayout>
-          <Component {...pageProps} />
-        </AppLayout>
-      </ChakraColorWrapper>
-    </ChakraProvider>
+    <ChakraColorWrapper cookies={pageProps.cookies}>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </ChakraColorWrapper>
   );
 }
 
--- src/components/ChakraColorWrapper.tsx
+++ src/components/ChakraColorWrapper.tsx
@@ -1,6 +1,8 @@
+import { theme } from "@chakra-ui/pro-theme";
 import {
   ChakraProvider,
   cookieStorageManager,
+  extendTheme,
   localStorageManager,
 } from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
@@ -15,19 +17,27 @@ export function ChakraColorWrapper({
   cookies,
   children,
 }: ChakraProps): JSX.Element {
+  const myTheme = extendTheme(
+    {
+      colors: { ...theme.colors, brand: theme.colors.purple },
+    },
+    theme
+  );
+
   const colorModeManager =
     typeof cookies === "string"
       ? cookieStorageManager(cookies)
       : localStorageManager;
 
   return (
-    <ChakraProvider colorModeManager={colorModeManager}>
+    <ChakraProvider colorModeManager={colorModeManager} theme={myTheme}>
       {children}
     </ChakraProvider>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  console.log("hello");
   return {
     props: {
       // first time users will not have any cookies, and you may not return
