9802959d32c2c8961aafe457da1f120180775a53
Anders Bech Mellson <mellson@icloud.com>
2022-04-02 14:15:24 +0200

    redirect til forsiden efter login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,16 @@
 import { Container } from "@chakra-ui/react";
 import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
 import { Auth, useUser } from "@supabase/supabase-auth-helpers/react";
+import { useRouter } from "next/router";
 
 export default function Login() {
   const { user, error } = useUser();
+  const router = useRouter();
+
+  if (user) {
+    router.push("/");
+  }
+
   return (
     <Container maxW="640px">
       {error && <p>{error.message}</p>}
