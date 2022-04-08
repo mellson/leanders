2c49767fa0658915cbfe06fd312698be6eb934cb
Anders Bech Mellson <mellson@icloud.com>
2022-04-08 16:37:06 +0200

    vis email

--- pages/profil.tsx
+++ pages/profil.tsx
@@ -1,4 +1,4 @@
-import { Button, Heading } from "@chakra-ui/react";
+import { Button, Heading, Text } from "@chakra-ui/react";
 import { User, withAuthRequired } from "@supabase/supabase-auth-helpers/nextjs";
 import NextLink from "next/link";
 
@@ -10,6 +10,7 @@ export default function Profil({ user }: ProfilProps) {
   return (
     <>
       <Heading size="md">Profil</Heading>
+      <Text>{user.email}</Text>
       <NextLink href="/api/auth/logout" passHref>
         <Button variant="outline">Log ud</Button>
       </NextLink>
