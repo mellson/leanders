9fcb28b107961dc805f2dff435185d52ab32286d
Anders Bech Mellson <mellson@icloud.com>
2022-04-08 16:43:04 +0200

    jura urls

---
+++ pages/betingelser.tsx
@@ -0,0 +1,10 @@
+import { Heading, Text } from "@chakra-ui/react";
+
+export default function Om() {
+  return (
+    <>
+      <Heading size="md">Vilkår og betingelser</Heading>
+      <Text>Du skal opføre dig ordentligt.</Text>
+    </>
+  );
+}
---
+++ pages/privatlivs-politik.tsx
@@ -0,0 +1,13 @@
+import { Heading, Text } from "@chakra-ui/react";
+
+export default function Om() {
+  return (
+    <>
+      <Heading size="md">Privatlivspolitik</Heading>
+      <Text>
+        Vi bruger din email for at give dig mulighed for at bestille brød og se
+        dine tidligere ordrer.
+      </Text>
+    </>
+  );
+}
