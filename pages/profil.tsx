c5648c6b9fc8e1edb83860c3356d4294b22b2842
Anders Bech Mellson <mellson@icloud.com>
2022-05-21 18:55:00 +0200

    isAdmin på profil siden

--- pages/profil.tsx
+++ pages/profil.tsx
@@ -1,17 +1,27 @@
 import { Button, Heading, Text } from "@chakra-ui/react";
-import { User, withPageAuth } from "@supabase/supabase-auth-helpers/nextjs";
+import {
+  supabaseServerClient,
+  User,
+  withPageAuth,
+} from "@supabase/supabase-auth-helpers/nextjs";
 import NextLink from "next/link";
 
 interface ProfilProps {
   user: User;
+  isAdmin: boolean;
 }
 
-export default function Profil({ user }: ProfilProps) {
+export default function Profil({ user, isAdmin }: ProfilProps) {
   return (
     <>
       <Heading size="md">Profil</Heading>
       <Text>{user.email}</Text>
       <Text>{user.id}</Text>
+      {isAdmin && (
+        <NextLink href="/ordrer" passHref>
+          <Button variant="outline">Kunders ordrer</Button>
+        </NextLink>
+      )}
       <NextLink href="/api/auth/logout" passHref>
         <Button variant="outline">Log ud</Button>
       </NextLink>
@@ -21,4 +31,10 @@ export default function Profil({ user }: ProfilProps) {
 
 export const getServerSideProps = withPageAuth({
   redirectTo: "/login?returnTo=profil",
+  async getServerSideProps(ctx) {
+    const { data } = await supabaseServerClient(ctx).from("admins").select("*");
+    const isAdmin = Array.isArray(data) && data.length > 0;
+
+    return { props: { isAdmin } };
+  },
 });
