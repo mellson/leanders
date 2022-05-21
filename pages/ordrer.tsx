b77a21b9b1eaebabd741f299b4e7b8500aa732fe
Anders Bech Mellson <mellson@icloud.com>
2022-05-21 15:58:43 +0200

    på vej med ordrer siden

--- pages/ordrer.tsx
+++ pages/ordrer.tsx
@@ -1,12 +1,14 @@
-import { definitions } from "@/types/supabase";
+import ChakraNextImage from "@/components/ChakraNextImage";
 import {
+  Checkbox,
   Heading,
+  HStack,
   Table,
   TableCaption,
   TableContainer,
   Tbody,
   Td,
-  Tfoot,
+  Text,
   Th,
   Thead,
   Tr,
@@ -16,11 +18,23 @@ import {
   User,
   withPageAuth,
 } from "@supabase/supabase-auth-helpers/nextjs";
+import { isToday } from "date-fns";
 import { useRouter } from "next/router";
+
+interface OrdreLinje {
+  id: number;
+  antal: number;
+  dato: string;
+  firma?: string;
+  user_email: string;
+  vare: string;
+  billede: string;
+  afsluttet: boolean;
+}
 
 interface ProfilProps {
   user: User;
-  ordrer: definitions["ordrer"];
+  ordrer: OrdreLinje[];
 }
 
 export default function Ordrer({ user, ordrer }: ProfilProps) {
@@ -30,45 +44,61 @@ export default function Ordrer({ user, ordrer }: ProfilProps) {
     router.push("/");
   }
 
-  console.log(ordrer);
-
   return (
     <>
       <Heading size="md">Ordrer</Heading>
       <TableContainer>
         <Table variant="simple">
-          <TableCaption placement="top">Dagens ordrer</TableCaption>
+          <TableCaption placement="top">Kommende ordrer</TableCaption>
           <Thead>
             <Tr>
-              <Th>To convert</Th>
-              <Th>into</Th>
-              <Th isNumeric>multiply by</Th>
+              <Th isNumeric>antal</Th>
+              <Th>vare</Th>
+              <Th>bestilt af</Th>
+              <Th>dato</Th>
+              <Th>håndteret</Th>
             </Tr>
           </Thead>
           <Tbody>
-            <Tr>
-              <Td>inches</Td>
-              <Td>millimetres (mm)</Td>
-              <Td isNumeric>25.4</Td>
-            </Tr>
-            <Tr>
-              <Td>feet</Td>
-              <Td>centimetres (cm)</Td>
-              <Td isNumeric>30.48</Td>
-            </Tr>
-            <Tr>
-              <Td>yards</Td>
-              <Td>metres (m)</Td>
-              <Td isNumeric>0.91444</Td>
-            </Tr>
+            {ordrer.map((linje) => {
+              const idag = isToday(new Date(linje.dato)) ? "brand.100" : "";
+              return (
+                <Tr
+                  key={linje.id}
+                  background={
+                    linje.afsluttet ? "gray.200" : idag ? "brand.100" : ""
+                  }
+                  opacity={linje.afsluttet ? 0.5 : 1}
+                  fontWeight={idag ? "bold" : ""}
+                  onClick={() => console.log("ordren er klikket på")}
+                  textDecoration={linje.afsluttet ? "line-through" : ""}
+                >
+                  <Td isNumeric>{linje.antal}</Td>
+                  <Td>
+                    <HStack justify="space-between">
+                      <Text>{linje.vare}</Text>
+                      <ChakraNextImage
+                        alt={linje.vare}
+                        src={`/billeder/${linje.billede}.jpeg`}
+                        width={60}
+                        height={60}
+                        transition="all 0.2s"
+                        _hover={{
+                          transform: "scale(1.05)",
+                          shadow: "md",
+                        }}
+                      />
+                    </HStack>
+                  </Td>
+                  <Td>{linje.firma ?? linje.user_email}</Td>
+                  <Td>{linje.dato}</Td>
+                  <Td>
+                    <Checkbox isChecked={linje.afsluttet} />
+                  </Td>
+                </Tr>
+              );
+            })}
           </Tbody>
-          <Tfoot>
-            <Tr>
-              <Th>To convert</Th>
-              <Th>into</Th>
-              <Th isNumeric>multiply by</Th>
-            </Tr>
-          </Tfoot>
         </Table>
       </TableContainer>
     </>
@@ -79,9 +109,9 @@ export const getServerSideProps = withPageAuth({
   redirectTo: "/login?returnTo=ordrer",
   async getServerSideProps(ctx) {
     const { data } = await supabaseServerClient(ctx)
-      .from<definitions["ordrer"]>("ordrer")
+      .from<OrdreLinje>("ordrer_view")
       .select("*")
-      .order("id");
+      .gte("dato", new Date().toDateString());
     return { props: { ordrer: data } };
   },
 });
