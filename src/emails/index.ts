e3ee9a6b93db8e969bd67e45f31f1651681c4e4d
Anders Bech Mellson <mellson@icloud.com>
2022-09-06 20:12:37 +0200

    svar ikke email update

--- src/emails/index.ts
+++ src/emails/index.ts
@@ -1,8 +1,6 @@
 import { buildSendMail } from 'mailing-core';
 import nodemailer from 'nodemailer';
 
-console.log('api: ' + process.env.SENDGRID_API_KEY);
-
 const transport = nodemailer.createTransport({
   host: 'smtp.sendgrid.net',
   port: 587,
@@ -14,7 +12,7 @@ const transport = nodemailer.createTransport({
 
 const sendMail = buildSendMail({
   transport,
-  defaultFrom: 'no_reply@leanders.dk',
+  defaultFrom: 'svar_ikke@leanders.dk',
 });
 
 export default sendMail;
