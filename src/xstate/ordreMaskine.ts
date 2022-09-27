b14544fe3071d7dfb8b0c55935f52122b1dda068
Anders Bech Mellson <mellson@icloud.com>
2022-09-27 14:52:46 +0200

    Update product images

--- src/xstate/ordreMaskine.ts
+++ src/xstate/ordreMaskine.ts
@@ -49,234 +49,232 @@ function getInitialContext(
 }
 
 export const ordreMaskine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QHkBOFVgAQFkCGsA1gJYB2YAdAIIBmAbmKQC5ioDEAKsQDY0AfAKyx08mRKAAOAe1jEmxKaXEgAHogCcANgCMFACzbNAdh1G96gKzaLegDQgAnogC0mvXooAOAEw+ADHqenpo6ftoAvuH2aBjY+ERklABqAGfcUFh4hPJ0WBB4TFJsAMopTJnZxLn5hcrSsvKKymoIAMzqRhRG2q2a3gEWRr3qdo4unkZ+Xr7eFp6Det7a85HR6Ji4BCTkFKnpFTl5BUW0AEaoDhB1MnIKSkiqiO2d3b39eoPDo04IzkaerWmPkWmj8ZnUs1WIBiG3i20oMOwUgkpwcUBgsE4PH4QhEYge9VuTQeLR6nnU+la3m8IyW2g6eiM9h+ziWFgoFjmWm8RiGJgsbihiM2CR2wuRqPRcBKZQOVSOtQJN0a91ApL0fimPNBdKpfnUYWZLlafm8HIsBr8nkZnjC+qF6ziW0SFHFKLRGJKTFE5QArhAEjRyllDjUpNcGndmohtH5jBRtHpWu1NBa5smjQh+uzjMtvJp1PMRtprA7YiL4a7HVgJR7pcVvahyvJeIIFeGlZHiWqYyXPBQ-IMLbbBtZApnnLGpj5PNpJoFeZr2mXYc6xdXa1LMQAhMCEVApIM1x0Romqx4IEveQHDA3W7nkzPuDy5nyF9T0kKtCwrp2ihEbu6W5sAAcr63CwC2x6xKeKrRpe2i+BQqaIR+H42IaYy-EMugzrMg53pyP5RNC1Zwi6ACqAYkEGrDtmwVGBsGlTVMcsFRiSTwdF0PR9AMQwFt8LjTr4hhxty-zfuo6i-hWlHUcQtGoPRxTcGAzGhmxnZnvBzw8W8-FfBOmitLoVL+PmokQrJ5E7IxNEsMpYZsGcFxXNpcGcW03GvHxHwCSMmbBAm3Q+JyVIQgC3g2WulBcK2Ah0c58U4u27HdheWi6AYximOYVg2BOKZAksQS8poEytDF-4UClghJccLk0OclzpeeLRZfohgmHO+VjhOib9jOc7JuorRGLMPjVZWu77oejnQZgbDIBImDlFIJ4eRxPa-OSUyGImPLXmEY2eMZGr6CM5IWFSFiDgE00urNB5KYtYBNS17mSMq20Xs4e0JjoiwTSa9KtGdWG3SFebWuDljfo9YqrepC0beWxAQGwECKJQZB0FIhAAeWtkIsjTCo9WGMIHjUgAMYFHcADafgALptfBzj5chBZzoYyYapyQm-AW7LmIEQQQjdFrRSRwok66ZMU+jmOsKgG0UBI3AFDQG0ALZVsTsUK2tSsbFTNP0yqzNs1tGUtJzljcx+3Qma0AuFVhzhUp0iE+D0YKzMswSI6TJt0WjGzcGQiXsNjOw04TBurjVK1h8pEfYFHpAx9TpD45bTOs+zXle7oJm8hM8wVQWwStMZhYlfmY3JjyVWy2RRupyj4fVlnMdsKr6ua9retJ3+lZd+TPfln3rC5-nDOKNbxc7aXyHjf8-wCsEhYmcZd2NxqrcnSHY+ZDQsDcL6U9MKB4GQTwb0rxeem+e8nyCROoUUOZ3iBC3SYJqnySMQLAAALUQWBGBYBoGAAQ3BlpkyfrbdqiAbAUANJg4IE1jBaAsBOdwmhkIzETBYWYN1DCRBIqQKQEA4DKDlkbWgDBmCsGfi0GuyEkwmikrxOMX9rw-xmFSchAoxrALSBkEM8owzsJjACLo7h5hxlMraboQt-rmEBtYAEfh2hBAMKfN0koMRyMvKZXQcxgg8i0LgsaA1BGcnJHOcSZC9GaFPvZRSC1ZEoPgssF4FVDCciGBqCqE4yFlxmPqfUplGRGFPnVGOaU-FeXKhyAwI4fD9DeBOXwFJzLLDjHo0KCN26Gxqs9ea098TfS7Kg34JZugDg-AEv+sxAiaHOmaP+gQOizFsVYIxisanYAxmYv4AohE2HGuNfUiFtD1ymGLckvIxZDE8MMtOb0sCz1QBMmk7IyHKIFOYFxeiBHsmkuLQI9IkzXiMdWPAF8r43zMUHRRgQ7omSKeor+gQhE+HGoMf4JY25rAqZWEB4DIHQNgfA95QRPnKJ+WoxMBCyHEL9r1UEOhNnlOTvCCZJZHbzLaYsGwwQJwjEBDOUyN0tBu18FQ8IQA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QHkBOFVgAQFkCGsA1gJYB2YAdAJKkykBqemqAxAMpgAuWEeneAIwLYAbkzCpEoAA4B7WMU7FZpKSAAeiALQAOAAwAmCgEYALMeMA2PZYCsAdguOANCACe26zpMHjBnfYAzPYAnAYhVgC+ka5oGNj4RGSUNHSMzCz0xLBYAAqo2RJqcgpKKmqaCLqGJuZWNg5Oxq4eVbZmFLa+gU4hgYG25tGx6Ji4BCTkFACCAGYiYKScEpnZeQWwRUggJYrKqtuVxnr9FKYhlpbGISGOjqYt2jqm9hSXBpamel-B9jo6wxAcTGiUmlDmCyWKwAKsQADazAAfACssGJMMV5HtyodECFbN57N97ET2h8dMZAo8qo4jO9bCFTJYbrZ+qZAcCEhNkhR6AAzuFQLB4QhKEQ8Piydh87gisUSziyTGlfYVRCBfEUPR6KzhAwGPSsrrUrR9bzvT46QIBMzGAExIGjLlJKb8wXC0XEcW8RUsOYCVBuCDK7EHUCVDW2LU6yx6g1GgwmnTtN6xyyBAymA3mAkcp3jF2UTlYWTSARuKAwWAsWEIlFo8QhsphjSISk6SwUewGwKGW7tL6WE3+QIUDX9LoEy7-Pp5+IFsEUYul8uVuCrHL5QqSba7ZtqhDGbvGLX4swGAl9L6J9zaa4n8e9yz-TOmQJ2Ocg7lTZdlitV6VZU9b1JSbVVcUPDVXlsT4+x0fELyHW8qmCPQxz6DUdHCPQrwMT9nUXX9VwAth+FQbgAFcICSWYgPlH0lV3LF9wg45PjOHoviNeCbmpSlXitew3zuXsjw7fCFx5Ij-3XUimG4JQ61RBiwJxcM2wsKNAlMGCbCwk4DXsE0rHNWNdN7GwDHfe0RnnUEpPzFcZOrAAhMBCFQPlaJLJ1VJbI4-DQuxQiPC9GVsWxqWCTtBNML49FCb4nwk+yf0cv812rAA5Ci4VgRSfPiPyDwsC8KD6PptUsP4YKM5CtFCEJUw+L4fhJf4Uu-SgAFVqJIWiJAVKVepouivSG4qIMjaNdTCeMBhvVotGfZqIsNHCDSsWxOsLCgRv65ZUCG9g4S4D16NApiVTU1sEGm7VZv1Q0FpNZkmt1BlXwsYxWR2xd9uIAajoYv1ZgDINJvUu7NQe2M5ue41kLtMdgjfBwDW1WwTkCP6eVrJFkUGkH8frFSrtDA98UJYlST8Z9KRNJlTGarxmRsY4HFxqYScJ4HJVB8Hg3JlioaprsacNOmKSperfE7el0wNS5zmMLnKDcjyvMOwrMBreECYbDFhfAqHdGuCh-BjLCLxeOwkxsMc9QCGCrNuVWHWLVL1fczygZ1sAWGQaRMG4WRfONm7Kl0MIKACbS7SZIkDFCE0iW8DMwmTfjbmCNWKA133tbD+IBcDIWZGYk3bujow4-MZ5qsMFPkIvV5+iE600xOQwcY9-MvaXYOuCL-NiAgFgIBUSgyBEWRCCLfuusHkOR-nMeEBn2QAGM+H2ABtPQAF1Ier8504pXUAh7nPU-TR2wj+IIsKw7a+7speg5Xwbi7GMeWAkVAYcKDSDhHwWYYcAC2S5F67U-sPb+o8IAb1ILPHezYD7Hwjv5bQZ8xwX1jFfV2QQTQamZhcD4hh-iDCzD9POcDOCrzGHCMgvMJ5TwoJvee0D36wKHgwhB85mGkF5sg1Bu8VAYJPlHbSY5zgkhCDhVCg5b7yzTFYISZkbB0L4Yw7AQjWEAKASAsBkDuFfl4V-I6P89EsIkKI7e4jSCSKwQeLQMjtK3ESn0Ikyj6qOFMh8ds14Lx4TfuYwi+Y8CzFgHCCi-DOAsBynlAq1ipHqhhjGOMCNFraD+J2XU6YKQ6XfIaPOWQsAAAsmBYEWFgWYYBkRwkDnw-2aSEBdBMG7BKaNniDEivVAcZwu5vmtDpc40QHSkFkBAOAahPZL1SIsdIWwK7XWwVUJwo59A9D+C8M0ydqTxVjqjeKiU3xaLCQRHkEJFiHTacyKMfRKR+AGDpFqqcIgmDTE9Z85wPhlIFEKOU40yarIpqxCkWoMzJgqnI8I-SloRE7CyW4-guj6CEnQ9KxFZkuNYr2bwARLjvm7DcOwOSNmfMfHpLCcV3yv1suEnkAM-agp2JXSObZkxahgkeEZwUPgmkvEMsK8EbAJXdoyq53N9YoiJpdMFItboki2eEIpBIuiGBIQ4L5rdvhWE+FYdklzJJTALlrARRtFVVyjps2OJwxJ7OtAcvxxwtShH8ESbsBJnjaMsf7LAY82kNQZFCoS-hrAkiZDLJa9gCT309S7KCkrHQ8MIjoy1NjhErPZWs1xaqLatQuP8aw3wKUNT6LHPUGiMwDBgli+cUSYlxOWJwNpdpOwGRhaQ1F+IkxWF1bGIICigg6TKcQSp1Tan1Mae2jsUK0Wwt7Qi7QmqRXWB+peZMoSpWmrAMGiwtx7U7OeKEZ1dUlrHDdRaAkfgzAVQmZEIAA */
   createMachine(
     {
-      preserveActionOrder: true,
-      predictableActionArguments: true,
-      context: getInitialContext([], true),
-      tsTypes: {} as import('./ordreMaskine.typegen').Typegen0,
-      schema: {
-        context: {} as OrdreMaskineContext,
-        events: {} as
-          | { type: 'Vis Priser'; visPriser: boolean }
-          | { type: 'Set database varer'; varer: definitions['varer'][] }
-          | { type: 'Tilføj vare'; vareId: number; antal: number; dato?: Date }
-          | { type: 'Sæt aktiv dato'; dato: Date }
-          | { type: 'Start udskift aktiv dato' }
-          | { type: 'Udskift aktiv dato'; dato: Date }
-          | { type: 'Slet aktiv dato' }
-          | { type: 'Start tilføj dato' }
-          | { type: 'Tilføj dato'; dato: Date }
-          | { type: 'Afbryd' }
-          | { type: 'Bekræft ordre' }
-          | { type: 'Opret ordre' }
-          | { type: 'Ordre oprettet' }
-          | { type: 'Nulstil ordre' }
-          | { type: 'Affyr Confetti' },
-        services: {} as {
-          'Opret ordre id': {
-            data: PostgrestResponse<definitions['ordrer']>;
-          };
-          'Opret ordre linjer': {
-            data: PostgrestResponse<
-              definitions['ordre_emails_der_ikke_er_sendt']
-            >;
-          };
+  context: getInitialContext([], true),
+  tsTypes: {} as import("./ordreMaskine.typegen").Typegen0,
+  schema: {
+    context: {} as OrdreMaskineContext,
+    events: {} as
+      | { type: "Vis Priser"; visPriser: boolean }
+      | { type: "Set database varer"; varer: definitions["varer"][] }
+      | { type: "Tilføj vare"; vareId: number; antal: number; dato?: Date }
+      | { type: "Sæt aktiv dato"; dato: Date }
+      | { type: "Start udskift aktiv dato" }
+      | { type: "Udskift aktiv dato"; dato: Date }
+      | { type: "Slet aktiv dato" }
+      | { type: "Start tilføj dato" }
+      | { type: "Tilføj dato"; dato: Date }
+      | { type: "Afbryd" }
+      | { type: "Bekræft ordre" }
+      | { type: "Opret ordre" }
+      | { type: "Ordre oprettet" }
+      | { type: "Nulstil ordre" }
+      | { type: "Affyr Confetti" },
+    services: {} as {
+      "Opret ordre id": {
+        data: PostgrestResponse<definitions["ordrer"]>;
+      };
+      "Opret ordre linjer": {
+        data: PostgrestResponse<definitions["ordre_emails_der_ikke_er_sendt"]>;
+      };
+    },
+  },
+  preserveActionOrder: true,
+  predictableActionArguments: true,
+  id: "Ordre Maskine",
+  initial: "IngenVarer",
+  states: {
+    IngenVarer: {
+      description: "Vi mangler data på varerne fra Supabase",
+      on: {
+        "Set database varer": {
+          actions: "Tilføj database varer til context",
+          target: "Afventer",
         },
-      },
-      id: 'Ordre Maskine',
-      initial: 'IngenVarer',
-      states: {
-        IngenVarer: {
-          description: 'Vi mangler data på varerne fra Supabase',
-          on: {
-            'Set database varer': {
-              actions: 'Tilføj database varer til context',
-              target: 'Afventer',
-            },
-            'Vis Priser': {
-              actions: 'Vis Priser',
-            },
-          },
-        },
-        Afventer: {
-          description: 'Ordremaskinen afventer instrukser',
-          on: {
-            'Vis Priser': {
-              actions: 'Vis Priser',
-            },
-            'Tilføj vare': [
-              {
-                actions: 'Tilføj vare til ordre',
-                cond: 'Dato til vare er ok',
-                target: 'Ordre opbygges',
-              },
-              {
-                actions: 'Gem midlertidigt vare',
-                cond: 'Dato til vare er ikke ok',
-                target: 'Vælg aktiv dato',
-              },
-            ],
-          },
-        },
-        'Vælg aktiv dato': {
-          on: {
-            'Sæt aktiv dato': {
-              actions: 'Sæt aktiv dato',
-              target: 'Ordre opbygges',
-            },
-            Afbryd: {
-              target: 'Afventer',
-            },
-          },
-        },
-        'Ordre opbygges': {
-          description: 'Brugeren er i gang med at opbygge deres ordre',
-          on: {
-            'Tilføj vare': [
-              {
-                actions: 'Tilføj vare til ordre',
-                cond: 'Dato til vare er ok',
-              },
-              {
-                actions: 'Gem midlertidigt vare',
-                cond: 'Dato til vare er ikke ok',
-                target: 'Vælg aktiv dato',
-              },
-            ],
-            'Vis Priser': {
-              actions: 'Vis Priser',
-            },
-            'Sæt aktiv dato': {
-              actions: 'Sæt aktiv dato',
-            },
-            'Start udskift aktiv dato': {
-              actions: 'Opdater mulige datoer',
-              target: 'Udskifter dato',
-            },
-            'Start tilføj dato': {
-              actions: 'Nulstil mulige datoer',
-              target: 'Tilføjer dato',
-            },
-            'Bekræft ordre': {
-              target: 'Bekræfter ordre',
-            },
-            'Nulstil ordre': {
-              actions: 'Nulstil ordre',
-              target: 'Afventer',
-            },
-          },
-        },
-        'Udskifter dato': {
-          on: {
-            'Udskift aktiv dato': {
-              actions: 'Udskift aktiv dato',
-              target: 'Ordre opbygges',
-            },
-            'Slet aktiv dato': [
-              {
-                actions: 'Nulstil ordre',
-                cond: 'Kun varer på aktiv dato',
-                target: 'Afventer',
-              },
-              {
-                actions: 'Slet aktiv dato',
-                cond: 'Varer på flere datoer',
-                target: 'Ordre opbygges',
-              },
-            ],
-            Afbryd: {
-              target: 'Ordre opbygges',
-            },
-          },
-        },
-        'Tilføjer dato': {
-          on: {
-            'Tilføj dato': [
-              {
-                actions: 'Tilføj dato',
-                cond: 'Dato eksisterer ikke',
-                target: 'Ordre opbygges',
-              },
-              {
-                actions: 'Sæt aktiv dato',
-                cond: 'Dato eksisterer',
-                target: 'Ordre opbygges',
-              },
-            ],
-            Afbryd: {
-              target: 'Ordre opbygges',
-            },
-          },
-        },
-        'Bekræfter ordre': {
-          description: 'Afventer at brugeren accepter ordren',
-          on: {
-            'Tilføj vare': {
-              actions: 'Tilføj vare til ordre',
-            },
-            'Opret ordre': {
-              target: 'Opretter ordre id',
-            },
-            Afbryd: {
-              target: 'Ordre opbygges',
-            },
-          },
-        },
-        'Opretter ordre id': {
-          description: 'Opretter et ordre id hos Supabase',
-          invoke: {
-            src: 'Opret ordre id',
-            onDone: [
-              {
-                actions: 'setOrdreId',
-                target: 'Opretter ordre linjer',
-              },
-            ],
-            onError: [
-              {
-                actions: 'setFejl',
-                target: 'Vi har en fejl',
-              },
-            ],
-          },
-        },
-        'Opretter ordre linjer': {
-          description: 'Opretter ordre linjerne hos Supabase',
-          invoke: {
-            src: 'Opret ordre linjer',
-            onDone: [
-              {
-                target: 'Ordre afsluttet',
-              },
-            ],
-            onError: [
-              {
-                actions: 'setFejl',
-                target: 'Vi har en fejl',
-              },
-            ],
-          },
-        },
-        'Ordre afsluttet': {
-          entry: [send('Affyr Confetti'), send('Nulstil ordre')],
-          on: {
-            'Nulstil ordre': {
-              actions: 'Nulstil ordre',
-              target: 'Afventer',
-            },
-          },
-        },
-        'Vi har en fejl': {
-          on: {
-            'Opret ordre': {
-              target: 'Opretter ordre id',
-            },
-          },
+        "Vis Priser": {
+          actions: "Vis Priser",
         },
       },
     },
+    Afventer: {
+      description: "Ordremaskinen afventer instrukser",
+      on: {
+        "Vis Priser": {
+          actions: "Vis Priser",
+        },
+        "Tilføj vare": [
+          {
+            actions: "Tilføj vare til ordre",
+            cond: "Dato til vare er ok",
+            target: "Ordre opbygges",
+          },
+          {
+            actions: "Gem midlertidigt vare",
+            cond: "Dato til vare er ikke ok",
+            target: "Vælg aktiv dato",
+          },
+        ],
+      },
+    },
+    "Vælg aktiv dato": {
+      on: {
+        "Sæt aktiv dato": {
+          actions: "Sæt aktiv dato",
+          target: "Ordre opbygges",
+        },
+        Afbryd: {
+          target: "Afventer",
+        },
+      },
+    },
+    "Ordre opbygges": {
+      description: "Brugeren er i gang med at opbygge deres ordre",
+      on: {
+        "Tilføj vare": [
+          {
+            actions: "Tilføj vare til ordre",
+            cond: "Dato til vare er ok",
+          },
+          {
+            actions: "Gem midlertidigt vare",
+            cond: "Dato til vare er ikke ok",
+            target: "Vælg aktiv dato",
+          },
+        ],
+        "Vis Priser": {
+          actions: "Vis Priser",
+        },
+        "Sæt aktiv dato": {
+          actions: "Sæt aktiv dato",
+        },
+        "Start udskift aktiv dato": {
+          actions: "Opdater mulige datoer",
+          target: "Udskifter dato",
+        },
+        "Start tilføj dato": {
+          actions: "Nulstil mulige datoer",
+          target: "Tilføjer dato",
+        },
+        "Bekræft ordre": {
+          target: "Bekræfter ordre",
+        },
+        "Nulstil ordre": {
+          actions: "Nulstil ordre",
+          target: "Afventer",
+        },
+      },
+    },
+    "Udskifter dato": {
+      on: {
+        "Udskift aktiv dato": {
+          actions: "Udskift aktiv dato",
+          target: "Ordre opbygges",
+        },
+        "Slet aktiv dato": [
+          {
+            actions: "Nulstil ordre",
+            cond: "Kun varer på aktiv dato",
+            target: "Afventer",
+          },
+          {
+            actions: "Slet aktiv dato",
+            cond: "Varer på flere datoer",
+            target: "Ordre opbygges",
+          },
+        ],
+        Afbryd: {
+          target: "Ordre opbygges",
+        },
+      },
+    },
+    "Tilføjer dato": {
+      on: {
+        "Tilføj dato": [
+          {
+            actions: "Tilføj dato",
+            cond: "Dato eksisterer ikke",
+            target: "Ordre opbygges",
+          },
+          {
+            actions: "Sæt aktiv dato",
+            cond: "Dato eksisterer",
+            target: "Ordre opbygges",
+          },
+        ],
+        Afbryd: {
+          target: "Ordre opbygges",
+        },
+      },
+    },
+    "Bekræfter ordre": {
+      description: "Afventer at brugeren accepter ordren",
+      on: {
+        "Tilføj vare": {
+          actions: "Tilføj vare til ordre",
+        },
+        "Opret ordre": {
+          target: "Opretter ordre id",
+        },
+        Afbryd: {
+          target: "Ordre opbygges",
+        },
+      },
+    },
+    "Opretter ordre id": {
+      description: "Opretter et ordre id hos Supabase",
+      invoke: {
+        src: "Opret ordre id",
+        onDone: [
+          {
+            actions: "setOrdreId",
+            target: "Opretter ordre linjer",
+          },
+        ],
+        onError: [
+          {
+            actions: "setFejl",
+            target: "Vi har en fejl",
+          },
+        ],
+      },
+    },
+    "Opretter ordre linjer": {
+      description: "Opretter ordre linjerne hos Supabase",
+      invoke: {
+        src: "Opret ordre linjer",
+        onDone: [
+          {
+            target: "Ordre afsluttet",
+          },
+        ],
+        onError: [
+          {
+            actions: "setFejl",
+            target: "Vi har en fejl",
+          },
+        ],
+      },
+    },
+    "Ordre afsluttet": {
+      entry: [send("Affyr Confetti"), send("Nulstil ordre")],
+      on: {
+        "Nulstil ordre": {
+          actions: "Nulstil ordre",
+          target: "Afventer",
+        },
+      },
+    },
+    "Vi har en fejl": {
+      on: {
+        "Opret ordre": {
+          target: "Opretter ordre id",
+        },
+      },
+    },
+  },
+},
     {
       actions: {
         'Tilføj database varer til context': assign({
