cec91a2e2f034d4b6ba1190f864b71dcf309dc42
Anders Bech Mellson <mellson@icloud.com>
2022-10-11 20:49:32 +0200

    Supabase v2 upgrade and login theme

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,9 @@
-import { PageBox } from "@/components/PageBox";
-import { Container } from "@chakra-ui/react";
-import { useSessionContext } from "@supabase/auth-helpers-react";
-import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { PageBox } from '@/components/PageBox';
+import { Container } from '@chakra-ui/react';
+import { useSessionContext } from '@supabase/auth-helpers-react';
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Login() {
   const { session, error, supabaseClient } = useSessionContext();
@@ -12,36 +12,50 @@ export default function Login() {
 
   useEffect(() => {
     if (session?.user) {
-      const hasReturnTo = returnTo && typeof returnTo === "string";
-      router.push(hasReturnTo ? `/${returnTo}` : "/");
+      const hasReturnTo = returnTo && typeof returnTo === 'string';
+      router.push(hasReturnTo ? `/${returnTo}` : '/');
     }
   }, [returnTo, router, session]);
 
   return (
     <PageBox>
-      <Container maxW={{ base: "full", md: "640px" }}>
+      <Container maxW={{ base: 'full', md: '640px' }}>
         {error && <p>{error.message}</p>}
 
         <Auth
           supabaseClient={supabaseClient}
-          providers={["facebook", "google"]}
+          providers={['facebook', 'google']}
           socialLayout="horizontal"
-          appearance={{ theme: ThemeSupa }}
+          appearance={{
+            theme: ThemeSupa,
+            variables: {
+              default: {
+                radii: {
+                  inputBorderRadius: '0',
+                  borderRadiusButton: '0',
+                  buttonBorderRadius: '0',
+                },
+                colors: {
+                  inputBackground: '#fff',
+                },
+              },
+            },
+          }}
           localization={{
             variables: {
               sign_in: {
-                email_label: "Email adresse",
-                password_label: "Kode",
-                button_label: "Log ind",
-                link_text: "Har du allerede en konto? Log ind her",
+                email_label: 'Email adresse',
+                password_label: 'Kode',
+                button_label: 'Log ind',
+                link_text: 'Har du allerede en konto? Log ind her',
               },
               forgotten_password: {
-                link_text: "Glemt kode?",
+                link_text: 'Glemt kode?',
               },
               sign_up: {
-                link_text: "Har du ikke en bruger? Opret en her",
-                email_label: "Email adresse",
-                password_label: "Kode",
+                link_text: 'Har du ikke en bruger? Opret en her',
+                email_label: 'Email adresse',
+                password_label: 'Kode',
               },
             },
           }}
