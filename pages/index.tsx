d1c1921dd774fe3138bcb21445fb2d58f97b0d6d
Anders Bech Mellson <mellson@icloud.com>
2022-10-19 19:29:07 +0200

    Opening hours

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import {
   Box,
   Button,
   Heading,
+  Link,
   SimpleGrid,
   Text,
   VStack,
@@ -38,38 +39,83 @@ export default function Home() {
 
   return (
     <>
-      <VStack h={["200px", "300px", "400px", "500px"]} justify="space-around">
-        <ChakraNextImage
-          alt="Cover foto"
-          src="/billeder/cover.jpeg"
-          position="absolute"
-          layout="fill"
-          w="full"
-          h={["200px", "300px", "400px", "500px"]}
-          zIndex={-1}
-        />
-        <Heading
-          size={{ base: "md", md: "lg", lg: "xl" }}
-          textAlign="center"
-          color="leanders.500"
-        >
-          Langsomt & lækkert siden 2017
-        </Heading>
-        <NextLink href="/bestil" passHref>
-          <Button
-            rounded="none"
-            color="leanders.900"
-            colorScheme="leanders"
-            size="lg"
-            fontWeight="bold"
-            textTransform="uppercase"
+      <Box
+        position="absolute"
+        left={0}
+        right={0}
+        h={["200px", "300px", "400px", "500px"]}
+      >
+        <VStack h="full" justify="space-between">
+          <ChakraNextImage
+            alt="Cover foto"
+            src="/billeder/cover.jpeg"
+            position="absolute"
+            layout="fill"
+            w="full"
+            h="full"
+            zIndex={-1}
+          />
+          <Heading
+            size={{ base: "md", md: "lg", lg: "xl" }}
+            textAlign="center"
+            color="leanders.500"
+            pt={{ base: 4, md: 20, lg: 40 }}
           >
-            Bestil her!
-          </Button>
-        </NextLink>
-      </VStack>
+            Langsomt & lækkert
+            <Box as="span" display={{ base: "none", sm: "inline" }}>
+              {" "}
+              siden 2017
+            </Box>
+          </Heading>
+          <NextLink href="/bestil" passHref>
+            <Button
+              rounded="none"
+              color="leanders.900"
+              colorScheme="leanders"
+              size="lg"
+              fontWeight="bold"
+              textTransform="uppercase"
+            >
+              Bestil her!
+            </Button>
+          </NextLink>
+          <Text
+            bg="rgba(231, 224, 212, 0.7)"
+            p={3}
+            w="full"
+            fontWeight="bold"
+            textAlign="center"
+            noOfLines={1}
+            overflow="hidden"
+            textOverflow="ellipsis"
+            whiteSpace="nowrap"
+          >
+            <Box as="span" display={{ base: "none", sm: "inline" }}>
+              TELEFON:
+            </Box>
+            <NextLink href="tel:+4525330045" passHref>
+              <Link>+45 25 33 00 45</Link>
+            </NextLink>
+            <Box as="span" display={{ base: "none", sm: "inline" }}>
+              {" "}
+              • ÅBNINGSTIDER:
+            </Box>
+            <Box as="span" display={{ base: "inline", sm: "none" }}>
+              {" "}
+              •{" "}
+            </Box>
+            Man - fre kl. 7-13 og lør kl. 7-11
+          </Text>
+        </VStack>
+      </Box>
 
-      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={16} py={16} px={8}>
+      <SimpleGrid
+        columns={{ base: 1, md: 2 }}
+        spacing={16}
+        py={16}
+        px={8}
+        mt={["200px", "300px", "400px", "500px"]}
+      >
         <TextWithHeading heading="Det gode brød">
           Hvad er et godt brød? Det er der jo mange meninger om, men her hos
           Brødkom- pagniet er det de gode økologiske råvarer og god tid, som
