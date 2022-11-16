ef190212a522c7c4d08d74426b9bc1e0c2cdee4c
Anders Bech Mellson <mellson@icloud.com>
2022-11-16 09:34:43 +0100

    Added loading spinner

--- pages/nulstil-kode.tsx
+++ pages/nulstil-kode.tsx
@@ -1,10 +1,21 @@
 import NulstilKode from '@/components/nulstilKode';
 import { PageBox } from '@/components/PageBox';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Spinner } from '@chakra-ui/react';
 import { useSessionContext } from '@supabase/auth-helpers-react';
 
 export default function NulstilKodePage() {
-  const { session } = useSessionContext();
+  const { isLoading, session } = useSessionContext();
+
+  if (isLoading)
+    return (
+      <PageBox>
+        <Heading as="h3" size="sm" textTransform="uppercase">
+          Gør siden klar...
+        </Heading>
+        <Spinner />
+      </PageBox>
+    );
+
   if (!session?.user)
     return (
       <PageBox>
